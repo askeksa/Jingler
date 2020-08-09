2a13363b6f681e0882d8a9349bd7e01e084a972c
Aske Simon Christensen <askesc@gmail.com>
2020-08-09 19:11:06 +0200

    Adjusted instruction capacities

--- bytecode/src/encode.rs
+++ bytecode/src/encode.rs
@@ -52,12 +52,12 @@ pub fn encode_bytecodes(bytecodes: &[Bytecode], sample_rate: f32) -> Result<(Vec
 	let FDONE = next(1);
 	let FOP = next(15);
 	let PROC = next(1);
-	let PROC_CALL = next(50);
+	let PROC_CALL = next(75);
 	let CONSTANT = next(50);
 	let NOTE_PROPERTY = next(3);
-	let STACK_LOAD = next(25);
-	let STACK_STORE = next(25);
-	let CELL_STORE = next(25);
+	let STACK_LOAD = next(20);
+	let STACK_STORE = next(20);
+	let CELL_STORE = next(10);
 	let LABEL = next(1);
 	let IF = next(1);
 	let LOOPJUMP = next(1);
@@ -66,13 +66,15 @@ pub fn encode_bytecodes(bytecodes: &[Bytecode], sample_rate: f32) -> Result<(Vec
 	let ROUND = next(4);
 	let COMPARE = next(7);
 
-	fn check(value: u16, capacity: u16, name: &str) -> Result<u8, String> {
-		if value < capacity {
-			Ok(value as u8)
-		} else {
-			Err(format!("Exceeded {} capacity.", name))
+	let mut exceeded = BTreeMap::new();
+	let mut check = |value: u16, capacity: u16, name: &'static str| -> u8 {
+		if value >= capacity {
+			exceeded.entry(name).and_modify(|existing: &mut (u16, u16)| {
+				existing.0 = value.max(existing.0)
+			}).or_insert((value, capacity));
 		}
-	}
+		value as u8
+	};
 
 	let mut encoded = vec![];
 	for &bc in bytecodes {
@@ -93,13 +95,13 @@ pub fn encode_bytecodes(bytecodes: &[Bytecode], sample_rate: f32) -> Result<(Vec
 			Bytecode::Fdone => FDONE,
 			Bytecode::Fop(opcode) => FOP + (0xFF - opcode as u8),
 			Bytecode::Proc => PROC,
-			Bytecode::Call(proc) => PROC_CALL + check(proc, 50, "procedure")?,
-			Bytecode::Constant(constant) => CONSTANT + check(constant_map[&constant], 50, "constant")?,
-			Bytecode::SampleRate => CONSTANT + check(constant_map[&sample_rate.to_bits()], 50, "constant")?,
+			Bytecode::Call(proc) => PROC_CALL + check(proc, 75, "procedure"),
+			Bytecode::Constant(constant) => CONSTANT + check(constant_map[&constant], 50, "constant"),
+			Bytecode::SampleRate => CONSTANT + check(constant_map[&sample_rate.to_bits()], 50, "constant"),
 			Bytecode::ReadNoteProperty(property) => NOTE_PROPERTY + property as u8,
-			Bytecode::StackLoad(offset) => STACK_LOAD + check(offset, 25, "stack load")?,
-			Bytecode::StackStore(offset) => STACK_STORE + check(offset, 25, "stack store")?,
-			Bytecode::CellStore(offset) => CELL_STORE + check(offset, 25, "cell store")?,
+			Bytecode::StackLoad(offset) => STACK_LOAD + check(offset, 20, "stack load"),
+			Bytecode::StackStore(offset) => STACK_STORE + check(offset, 20, "stack store"),
+			Bytecode::CellStore(offset) => CELL_STORE + check(offset, 10, "cell store"),
 			Bytecode::Label => LABEL,
 			Bytecode::If => IF,
 			Bytecode::Loop => LOOPJUMP,
@@ -129,6 +131,14 @@ pub fn encode_bytecodes(bytecodes: &[Bytecode], sample_rate: f32) -> Result<(Vec
 			Bytecode::Max => implicit_code(0x5F),
 		};
 		encoded.push(e);
+	}
+
+	if !exceeded.is_empty() {
+		let mut errors = String::new();
+		for (name, (value, capacity)) in exceeded {
+			errors += &format!("\nExceeded {} capacity ({} > {}).", name, value + 1, capacity);
+		}
+		return Err(errors);
 	}
 
 	Ok((encoded, constants))
