c88e68edbb2c5a91bf67f575a63a9e97f69bfdad
Aske Simon Christensen <askesc@gmail.com>
2020-08-16 20:10:40 +0200

    Mark nasm build step as dependent on all relevant asm files

--- zing-vst/build.rs
+++ zing-vst/build.rs
@@ -7,5 +7,9 @@ fn main() {
 	#[cfg(target_env = "msvc")]
 	nasm_rs::compile_library("clinklang_cmd.lib", &["../runtime/cmd.asm"]);
 
+	println!("cargo:rerun-if-changed=../runtime/cmd.asm");
+	println!("cargo:rerun-if-changed=../runtime/clinklang.asm");
+	println!("cargo:rerun-if-changed=../runtime/used_instructions.inc");
+
 	println!("cargo:rustc-link-lib=static=clinklang_cmd");
 }
