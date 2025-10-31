fbfb9b9bc243c9eebc87f796c1b8f900022d4625
Aske Simon Christensen <askesc@gmail.com>
2025-10-31 15:06:42 +0100

    Adjusted path to runtime

--- runtime/cmd.asm
+++ runtime/cmd.asm
@@ -1,6 +1,6 @@
 
-%include "../runtime/used_instructions.inc"
-%include "../runtime/jingler.asm"
+%include "../../runtime/used_instructions.inc"
+%include "../../runtime/jingler.asm"
 
 extern __imp__VirtualAlloc@16
 extern __imp__VirtualFree@12
