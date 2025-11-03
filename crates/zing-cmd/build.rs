ed7f686158c1d414f2c6ab567eebc5175e9cec85
Aske Simon Christensen <askesc@gmail.com>
2025-11-03 21:55:38 +0100

    Adjusted path to runtime in zing-cmd

--- crates/zing-cmd/build.rs
+++ crates/zing-cmd/build.rs
@@ -3,14 +3,14 @@ extern crate nasm_rs;
 #[cfg(target_vendor = "pc")]
 fn main() {
 	#[cfg(not(target_env = "msvc"))]
-	nasm_rs::compile_library("libjingler_cmd.a", &["../runtime/cmd.asm"]);
+	nasm_rs::compile_library("libjingler_cmd.a", &["../../runtime/cmd.asm"]);
 
 	#[cfg(target_env = "msvc")]
-	nasm_rs::compile_library("jingler_cmd.lib", &["../runtime/cmd.asm"]);
+	nasm_rs::compile_library("jingler_cmd.lib", &["../../runtime/cmd.asm"]);
 
-	println!("cargo:rerun-if-changed=../runtime/cmd.asm");
-	println!("cargo:rerun-if-changed=../runtime/jingler.asm");
-	println!("cargo:rerun-if-changed=../runtime/used_instructions.inc");
+	println!("cargo:rerun-if-changed=../../runtime/cmd.asm");
+	println!("cargo:rerun-if-changed=../../runtime/jingler.asm");
+	println!("cargo:rerun-if-changed=../../runtime/used_instructions.inc");
 
 	println!("cargo:rustc-link-lib=static=jingler_cmd");
 }
